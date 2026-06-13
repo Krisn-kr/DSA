75017209e8017bee180e38700a34d42b2ff94850
Krisn-18 <krishna0018kp@gmail.com>
2026-06-13 21:40:49 +0530

    Initial commit

---
+++ explain_pairs.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void explainPairs() {
+    pair<int,int>p={1,2};
+    
+    cout<<p.first<<" "<<p.second;
+
+}
+
+int main(){
+
+    explainPairs();
+
+    return 0;
+}
+
+
