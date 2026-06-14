21827e3c7e34acf1efa3681038247ed43825921d
Krisn-18 <krishna0018kp@gmail.com>
2026-06-14 09:26:41 +0530

    learning Deque

--- List/list.cpp
+++ List/list.cpp
@@ -2,6 +2,7 @@
 #include<string>
 #include<list>
 using namespace std;
+// ================================
 
 void explain_list(){
     list<int> ls;
@@ -14,12 +15,41 @@ void explain_list(){
     for(auto it : ls){
         cout<<it<<" ";
     }
+    cout<<"\n";
 
 
+} 
+
+// ======================
+void explainDeque(){
+    deque<int> dq;
+    dq.push_back(1); // {1}
+    dq.emplace_back(2);  // {1, 2}
+
+    dq.push_front(3); //{3, 1, 2}
+    dq.emplace_front(4); //{4, 3, 1, 2}
+
+
+    for(auto it : dq){
+        cout<<it<<", ";
+
+    
+ 
+    }cout<<"\n";
+
+
+    dq.back();
+    dq.front();
+
+    //  rest functions same as vector
+    // begin, end, rbegin, rend, clear, insert, size, swap
 }
 
 int main(){
     cout<<" list \n";
     explain_list();
+
+    cout<<"Deque ========\n";
+    explainDeque();
     return 0;
 }
