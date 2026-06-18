8661793dbf3c81a8f7221ff4c5f9a02d927a56df
Krisn-18 <krishna0018kp@gmail.com>
2026-06-18 23:34:40 +0530

    multi map

--- c++_Stl/Map/Multimap.cpp
+++ c++_Stl/Map/Multimap.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 #include<map>
 using namespace std;
+
+// multi map
+
+void explainMultiMap(){
+    // everything same as map, only it can store multiple keys
+    // only mapp[key] cannot be used here
+    // store duplicate keys
+}
 int main(){
     return 0;
 }
