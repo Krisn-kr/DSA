bae87aacce3ffb4723cb4bf2f96a5b9fad6c46ef
Krisn-18 <krishna0018kp@gmail.com>
2026-06-19 09:15:17 +0530

    Reverse Number

--- BasicMaths/Maths.java
+++ BasicMaths/Maths.java
@@ -1,7 +1,13 @@
 package BasicMaths;
-
+import java.util.*;
+// import 
 public class Maths {
 
+
+    // ========================================
+    //  O(log 10 (N))
+    // ========================================
+    
     static void extractDigits(){
         int n = 1234;
         int digit;
@@ -12,9 +18,34 @@ public class Maths {
 
 
         }
+
+        // or
+
+        // int cnt = (int)(Math.log10(n)+1);
+        // System.out.println(cnt);
+    }
+
+    // Reverse number
+
+    static void reverseNumber(){
+        int n = 12345;
+        int rNum=0;
+
+        while(n>0){
+            int digit = n%10;
+            rNum = rNum *10 + digit;
+            n/=10;
+
+
+
+        }
+        System.out.println("Reverse Number : "+ rNum);
+
+        
     }
     public static void main(String[] args) {
         extractDigits();
+        reverseNumber();
         
     }
 }
