b85d74e5d71f5419c58a74d17a75f732d8ab7206
Krisn-18 <krishna0018kp@gmail.com>
2026-07-26 09:13:53 +0530

    Bruteforce approach

--- LeetCode/MaxSubArray.java
+++ LeetCode/MaxSubArray.java
@@ -1,31 +1,29 @@
 package LeetCode;
 
+
+// BruteForce approach
 public class MaxSubArray {
-
-    public static void maxSubArray(int[] nums){
-
+    public static int maxSubArray(int[] nums){
         int maxSum = 0;
-
         for(int i =0; i< nums.length; i++){
             // subaaray
             for(int j = i ; j<nums.length; j++){
-
                 int sum = 0;
                 for(int k = i ;k <= j ; k++){
-                    System.out.print(k+" ");
+                    // updating sum
+                    sum += nums[k];
+                    maxSum = Math.max(maxSum,sum );
                 }
-                System.out.println();
-
             }
         }
 
-//   return 
+  return  maxSum;
     }
 
     public void main(String[] args) {
         int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
-        // System.out.println(maxSubArray(nums));
-        maxSubArray(nums);
+        System.out.println(maxSubArray(nums));
+
     }
     
 }
