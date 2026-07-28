9bf3488843bf15a0a16468227d352db99ddca49b
Krisn-18 <krishna0018kp@gmail.com>
2026-07-28 22:51:24 +0530

    Best Time to buy and sell Stock

---
+++ LeetCode/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,33 @@
+package LeetCode;
+
+import java.util.Scanner;
+import java.util.Vector;
+
+public class BestTimeToBuyAndSellStock {
+
+    public static int BuyAndSell(Vector<Integer> nums){
+        int profit = 0;
+        int buy = nums.get(0);
+        for(int i = 1; i < nums.size(); i++){
+            if(nums.get(i) > nums.get(i-1)){
+                profit = profit + (nums.get(i) - nums.get(i-1));
+            }    
+            }
+        
+        return profit ;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Vector<Integer> nums = new Vector<>();
+        System.out.print("Enter the number of the stock prices : ");
+        int n = sc.nextInt();
+        System.out.print("Enter the Price flactuation of the stock : ");
+        for(int i = 0; i< n;i++){
+            nums.add(sc.nextInt());
+        }
+
+        // Calling the vector
+        System.out.println(BuyAndSell(nums));
+        sc.close();
+    }
+}
