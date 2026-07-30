22315d267f9aee30560b8621246bf14df3b09061
Krisn-18 <krishna0018kp@gmail.com>
2026-07-30 15:22:12 +0530

    Bugs

---
+++ LeetCode/ContainerMostWater.java
@@ -0,0 +1,18 @@
+package LeetCode;
+
+import java.util.Scanner;
+
+public class ContainerMostWater {
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of columns : ");
+        int col = sc.nextInt();
+        int []arr= new int[col];
+
+        // Array
+        for(int i = 0; i < col; i++){
+            System.out.println("Enter the height of column "+i+1);
+            arr[i] = sc.nextInt();
+        }
+    }
+}
