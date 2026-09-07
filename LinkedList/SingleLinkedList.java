b832d33359102798379356320dd11d79692afe81
Krisn-18 <krishna0018kp@gmail.com>
2026-09-07 21:55:52 +0530

    Bugs

--- LinkedList/SingleLinkedList.java
+++ LinkedList/SingleLinkedList.java
@@ -1,4 +1,6 @@
 package LinkedList;
+
+// node creation
 class Node{
         int data;
         Node next;
@@ -14,6 +16,8 @@ class Node{
             this.next = null;
         }
     };
+
+    //single linked list
     
 public class SingleLinkedList {
 
@@ -29,7 +33,7 @@ public class SingleLinkedList {
         return head;
     
         }     
-        
+        // Finding the length of the linked list
         private static int lengthOfLL(Node head){
             int cnt =0; 
             Node temp = head;
@@ -38,6 +42,23 @@ public class SingleLinkedList {
                 cnt++;
             }
             return cnt;
+        }
+
+        // check if the value is present 
+        public static String checkValue(Node head, int val){
+            Node temp = head;
+            int cnt =0;
+            while(temp != null){
+
+                if(temp.data == val){
+                    cnt++;
+                }
+                temp= temp.next;
+            }
+            if(cnt<1){
+                return "Not Present";
+            }
+            return "Present";
         }
     
     public static void main(String[] args) {
@@ -53,7 +74,13 @@ public class SingleLinkedList {
 
         }
         System.out.println("\n The length of the node : "+lengthOfLL(head));
+
+        // Check if the value is present or not;
+        System.out.println("Check 2 is present or not :\n" +checkValue(head,2));
     }
+
+    
+    
     
     
 }
