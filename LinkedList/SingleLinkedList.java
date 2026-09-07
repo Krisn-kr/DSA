3771e20140eac41a9d3c439e1e8dde7621e6127a
Krisn-18 <krishna0018kp@gmail.com>
2026-09-07 20:54:21 +0530

    Array to linked list

--- LinkedList/SingleLinkedList.java
+++ LinkedList/SingleLinkedList.java
@@ -13,13 +13,26 @@ class Node{
             this.data = data1;
             this.next = null;
         }
-    }
+    };
+    
 public class SingleLinkedList {
+
+    private static Node convertArr2LL(int[] arr){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+
+        for( int i = 1; i< arr.length; i++){
+        Node  temp = new Node(arr[i]);
+        mover.next = temp;
+        }
+        return head;
+    
+        }       
     
     public static void main(String[] args) {
         int[] arr = {2, 5, 6, 8};
-        Node y = new Node(arr[3]);
-        System.out.println(y.data);
+        Node head = convertArr2LL(arr);
+        System.out.println(head.data);
     }
     
 }
--- LinkedList/singlelinkedList.cpp
+++ LinkedList/singlelinkedList.cpp
@@ -10,12 +10,29 @@ class Node{
         data = data1;
         next = next1;
     } 
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    } 
 };
 
+Node * convertArr2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+
+    for( int i = 1; i< arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+    }
+     return head;
+    
+}
 int main(){
     vector<int> arr = {2, 5, 8, 7};
-    Node* y =  new Node(arr[0], nullptr);
+    Node* head = convertArr2LL(arr);
+    cout<< head->data;
 
-    cout<< y->data;
+
+    
     return 0;
 }
